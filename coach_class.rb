0e6f12d826beb54fbcb697d4ee6e1f661023e6b5
A_Lippok <alippok@hotmail.co.uk>
2016-02-23 20:08:02 +0000

    Adds method for coach to add qualifications to themself

--- coach_class.rb
+++ coach_class.rb
@@ -7,10 +7,15 @@ def initialize(name, role, qualies)
   @name = name
   @role = role
   @qualies = qualies
-
 end
 
 
+def add_qualifications(qualifications)
+  for qualification in qualifications
+    @qualies.push(qualification)
+  end
+  
+end
 
 
 
--- coach_class_spec.rb
+++ coach_class_spec.rb
@@ -6,7 +6,7 @@ class TestCoachClass < MiniTest::Test
 def setup
 @coach1 = Coach.new("Judy Farn", "Head coach", ["Rugby League Coaching Level 4", "Strength and Conditioning Level 5", "Food and Nutrition for Performance NA"]) #The @ is important
 #here to make sure it is available across the class
-
+@new_qualifications = ["Performance Standards National 5", "Managing Performers and Teams N8" ]
 end
 
 def test_name
@@ -21,6 +21,10 @@ def test_qualies
   assert_equal(["Rugby League Coaching Level 4", "Strength and Conditioning Level 5", "Food and Nutrition for Performance NA"], @coach1.qualies)
 end
 
+def test_add_own_qualifications
+  @coach1.add_qualifications(@new_qualifications)
+  assert_equal(["Rugby League Coaching Level 4", "Strength and Conditioning Level 5", "Food and Nutrition for Performance NA", "Performance Standards National 5", "Managing Performers and Teams N8" ], @coach1.qualies)
+end
 
 
 end
