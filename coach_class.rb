4ab60623608b3a6bded3e7496d94da19cd90e1b3
A_Lippok <alippok@hotmail.co.uk>
2016-02-23 19:48:38 +0000

    Adds tests to coach class

---
+++ coach_class.rb
@@ -0,0 +1,21 @@
+class Coach
+
+attr_reader(:name, :role, :qualies)
+
+
+def initialize(name, role, qualies)
+  @name = name
+  @role = role
+  @qualies = qualies
+
+end
+
+
+
+
+
+
+
+
+
+end 
---
+++ coach_class_spec.rb
@@ -0,0 +1,22 @@
+require("minitest/autorun")
+require_relative("./coach_class.rb")
+
+class TestCoachClass < MiniTest::Test
+
+def setup
+@coach1 = Coach.new("Judy Farn", "Head coach", ["Rugby League Coaching Level 4", "Strength and Conditioning Level 5", "Food and Nutrition for Performance NA"]) #The @ is important
+#here to make sure it is available across the class
+
+end
+
+def test_name
+  assert_equal("Judy Farn", @coach1.name)
+end
+
+def test_role
+  assert_equal("Head coach", @coach1.role)
+end
+
+
+
+end
